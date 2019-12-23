63c14e7438e3082fd20f36d5c82553d02e1a4454
Luiz Cartolano <duducartolano@gmail.com>
2019-12-23 18:59:58 -0300

    add sixth project

---
+++ extras/decision_tree.py
@@ -0,0 +1,26 @@
+# Import statements 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+# Read the data.
+data = np.asarray(pd.read_csv('data.csv', header=None))
+# Assign the features to the variable X, and the labels to the variable y. 
+X = data[:,0:2]
+y = data[:,2]
+
+# TODO: Create the decision tree model and assign it to the variable model.
+# You won't need to, but if you'd like, play with hyperparameters such
+# as max_depth and min_samples_leaf and see what they do to the decision
+# boundary.
+model = DecisionTreeClassifier()
+
+# TODO: Fit the model.
+model.fit(X,y)
+
+# TODO: Make predictions. Store them in the variable y_pred.
+y_pred = model.predict(X)
+
+# TODO: Calculate the accuracy and assign it to the variable acc.
+acc = accuracy_score(y, y_pred)
